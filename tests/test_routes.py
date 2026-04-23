66c7ec425480d7f995869a31de54d237c593f636
Victor-Osei <oseivictor10@yahoo.com>
2026-04-23 07:14:39 -0400

    implemented read account endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,48 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    ######################################################################
+    # READ TEST CASES
+    ######################################################################
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}",
+            content_type="application/json"
+        )
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+
+        self.assertEqual(data["id"], account.id)
+        self.assertEqual(data["name"], account.name)
+        self.assertEqual(data["email"], account.email)
+
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/9999")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_bad_request_empty_json(self):
+        """It should fail when empty JSON is sent"""
+        resp = self.client.post(
+            BASE_URL,
+            data={},
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_get_account_invalid_id_type(self):
+        """It should fail when ID type is invalid"""
+        resp = self.client.get(f"{BASE_URL}/invalid")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_method_not_allowed(self):
+        """It should fail when wrong HTTP method is used"""
+        resp = self.client.put("/health")  # PUT is not allowed here
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
