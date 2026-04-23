bdb52016306b74c2f89e8c28c24f326b4dc42a67
Victor-Osei <oseivictor10@yahoo.com>
2026-04-23 08:05:06 -0400

    completed CRUD REST API with 96% coverage

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,15 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """List all Accounts"""
+    app.logger.info("Request to list Accounts")
+
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
@@ -90,6 +99,20 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """Update an Account"""
+    app.logger.info("Request to update Account %s", account_id)
+
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, "Account not found")
+
+    account.deserialize(request.get_json())
+    account.update()
+
+    return account.serialize(), status.HTTP_200_OK
+
 
 
 ######################################################################
@@ -97,6 +120,17 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """Delete an Account"""
+    app.logger.info("Request to delete Account %s", account_id)
+
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
+
 
 
 ######################################################################
