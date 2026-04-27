638a615511d1e7e543879f19b735331d2da4b062
Victor-Osei <oseivictor10@yahoo.com>
2026-04-27 07:07:19 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -20,7 +20,7 @@ DATABASE_URI = os.getenv(
 )
 
 BASE_URL = "/accounts"
-HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+HTTPS_ENVIRON = {"wsgi.url_scheme": "https"}
 
 
 ######################################################################
@@ -95,9 +95,7 @@ class TestAccountService(TestCase):
         """It should Create a new Account"""
         account = AccountFactory()
         response = self.client.post(
-            BASE_URL,
-            json=account.serialize(),
-            content_type="application/json"
+            BASE_URL, json=account.serialize(), content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -122,9 +120,7 @@ class TestAccountService(TestCase):
         """It should not Create an Account when sending the wrong media type"""
         account = AccountFactory()
         response = self.client.post(
-            BASE_URL,
-            json=account.serialize(),
-            content_type="test/html"
+            BASE_URL, json=account.serialize(), content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
@@ -137,8 +133,7 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
 
         resp = self.client.get(
-            f"{BASE_URL}/{account.id}",
-            content_type="application/json"
+            f"{BASE_URL}/{account.id}", content_type="application/json"
         )
 
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
@@ -149,32 +144,25 @@ class TestAccountService(TestCase):
         self.assertEqual(data["name"], account.name)
         self.assertEqual(data["email"], account.email)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/9999")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-
     def test_bad_request_empty_json(self):
         """It should fail when empty JSON is sent"""
-        resp = self.client.post(
-            BASE_URL,
-            data={},
-            content_type="application/json"
-        )
+        resp = self.client.post(BASE_URL, data={}, content_type="application/json")
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_get_account_invalid_id_type(self):
         """It should fail when ID type is invalid"""
         resp = self.client.get(f"{BASE_URL}/invalid")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-    
-    def test_method_not_allowed(self):
+
+    def test_put_method_not_allowed(self):
         """It should fail when wrong HTTP method is used"""
         resp = self.client.put("/health")  # PUT is not allowed here
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
@@ -186,7 +174,6 @@ class TestAccountService(TestCase):
 
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -200,16 +187,12 @@ class TestAccountService(TestCase):
         new_account = resp.get_json()
         new_account["name"] = "Something Known"
 
-        resp = self.client.put(
-            f"{BASE_URL}/{new_account['id']}",
-            json=new_account
-        )
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
 
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
         updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "Something Known")    
-
+        self.assertEqual(updated_account["name"], "Something Known")
 
     def test_delete_account(self):
         """It should Delete an Account"""
@@ -219,34 +202,28 @@ class TestAccountService(TestCase):
 
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-    
-    def test_method_not_allowed(self):
+    def test_deletemethod_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    
 
     def test_security_headers(self):
         """It should return security headers"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         headers = {
-            'X-Frame-Options': 'SAMEORIGIN',
-            'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-            'Referrer-Policy': 'strict-origin-when-cross-origin'
+            "X-Frame-Options": "SAMEORIGIN",
+            "X-Content-Type-Options": "nosniff",
+            "Content-Security-Policy": "default-src 'self'; object-src 'none'",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
-    
     def test_cors_security(self):
         """It should return a CORS header"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Check for the CORS header
-        self.assertEqual(
-            response.headers.get('Access-Control-Allow-Origin'),
-            '*'
-        )    
+        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "*")
